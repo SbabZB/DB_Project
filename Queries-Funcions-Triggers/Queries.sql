5ac2a44a2f1f482624f69303454a6d99b517f25a
Sebastiano Bertolin <sebastiano.bertolin@outlook.com>
2016-12-29 21:49:07 +0000

    aggiunta query scambio personale scali

--- Queries-Funcions-Triggers/Queries.sql
+++ Queries-Funcions-Triggers/Queries.sql
@@ -14,3 +14,20 @@ END//
 
 DELIMITER ;
 
+/* Query per trovare il personale sbarcato in uno scalo x e il personale imbarcato al suo posto */
+SELECT K.Nave,K.MatricolaS,K.NomeS,K.CognomeS,K.Qualifica,K.Grado,K.MatricolaI,K.NomeI,K.NomeS,K.CognomeI,ETA,No_Viag,Operazione,Porto FROM(
+SELECT P_sbar.Nave as Nave, P_sbar.Matricola as MatricolaS, P_sbar.Nome as NomeS, P_sbar.Cognome as CognomeS,
+       P_imb.Matricola AS MatricolaI, P_imb.Nome AS NomeI, P_imb.Cognome AS CognomeI, P_imb.Qualifica, P_imb.Grado
+FROM (SELECT *
+      FROM Personale JOIN Equipaggio ON Matricola = Membro
+      WHERE Data_sbarco = "2016-02-22" AND Nave = "IMO5641147") as P_sbar
+JOIN (SELECT *
+      FROM Personale JOIN Equipaggio ON Matricola = Membro
+      WHERE Data_imbarco = "2016-02-22" AND Nave = "IMO5641147") as P_imb
+WHERE P_sbar.Qualifica = P_imb.Qualifica
+  AND P_sbar.Grado = P_imb.Grado) AS K
+  JOIN (SELECT Nave, CAST(ETA AS DATE) AS ETA, Numero_viaggio AS No_Viag, Operazione, Porto, CAST(Data_arrivo AS DATE) AS Data_arr,
+        CAST(Data_partenza AS DATE) AS Data_part FROM Scali) AS S
+  WHERE S.Nave = K.Nave
+  AND S.Data_arr = "2016-02-22"
+;
