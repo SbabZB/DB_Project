b3720d6963555768ab2c3ed88db054508d03b780
Crose37 <c.benetazzo37@gmail.com>
2017-01-10 16:47:28 +0100

    Aggiunta trigger controlloQualificaGrado

--- Queries-Funcions-Triggers/Triggers.sql
+++ Queries-Funcions-Triggers/Triggers.sql
@@ -2,7 +2,7 @@
 
 DROP TRIGGER IF EXISTS comp_viaggi;
 DELIMITER //
-CREATE TRIGGER comp_viaggi BEFORE INSERT ON Viaggi
+CREATE TRIGGER comp_viaggi AFTER INSERT ON Viaggi
 FOR EACH ROW
 BEGIN
 DECLARE comp VARCHAR(13);
@@ -36,3 +36,54 @@ IF NEW.Numero > num+1 THEN
 END IF;
 END//
 DELIMITER ;
+
+/*Controllo di pertinenza tra il grado e la qualficia dei nuovi aggiunti alla tabella Personale*/
+DROP TRIGGER IF EXISTS controlloQualificagrado;
+DELIMITER //
+CREATE TRIGGER controlloQualificagrado AFTER INSERT ON Personale
+FOR EACH ROW
+BEGIN
+
+DECLARE msg VARCHAR(128);
+SET msg = 'Inserito valore per Grado non coerente con Qualifica.';
+
+IF NEW.Qualifica = 'Cuoco' and NEW.Grado != 'Cuoco' or NEW.Qualifica =  'Cuoco' and NEW.Grado != 'Aiuto Cuoco'
+THEN
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = msg;
+
+ELSEIF NEW.Qualifica = 'Marinaio' and NEW.Grado != 'Mozzo' or NEW.Qualifica = 'Marinaio' and NEW.Grado != 'Marinaio Scelto'
+THEN
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = msg;
+
+ELSEIF NEW.Qualifica = 'Tecnico' and NEW.Grado != 'Operaio' or NEW.Qualifica = 'Tecnico' and NEW.Grado != 'Elettricista'
+THEN
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = msg;
+
+ELSEIF NEW.Qualifica = 'Ufficiale' and NEW.Grado != 'Secondo di Macchina'
+or NEW.Qualifica = 'Ufficiale' and NEW.Grado != 'Secondo di coperta'
+or NEW.Qualifica = 'Ufficiale' and NEW.Grado != 'Comandante'
+or NEW.Qualifica = 'Ufficiale' and NEW.Grado != 'Capo Macchinista'
+or NEW.Qualifica = 'Ufficiale' and NEW.Grado != 'Primo di coperta'
+or NEW.Qualifica = 'Ufficiale' and NEW.Grado != 'Primo di Macchina'
+or NEW.Qualifica = 'Ufficiale' and NEW.Grado != 'Allievo di Macchina'
+or NEW.Qualifica = 'Ufficiale' and NEW.Grado != 'Allievo di Coperta'
+THEN
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = msg;
+
+END IF;
+
+END//
+DELIMITER ;
+
+THEN
+SIGNAL SQLSTATE '45000'
+SET MESSAGE_TEXT = 'Inserito valore per Grado non coerente con Qualifica.';
+
+END IF
+END//
+
+DELIMITER;
